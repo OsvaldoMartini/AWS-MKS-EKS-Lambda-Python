f3847fef53ec28d4a3d87b90b16427911b15d4a7
Osvaldo Martini <martinio@dhl.com>
2023-09-17 14:26:20 +0200

    Update server.py

--- trivia-app/back-end-python/websocket-server/server.py
+++ trivia-app/back-end-python/websocket-server/server.py
@@ -1,9 +1,10 @@
 from websocket_server import WebsocketServer
+import json
 
 # Called for every client connecting (after handshake)
 def new_client(client, server):
 	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
+	server.send_message_to_all(json.dumps({"message": "All Connected"}))
 
 
 # Called for every client disconnecting
@@ -20,6 +21,7 @@ def message_received(client, server, message):
 
 PORT=9001
 server = WebsocketServer(port = PORT)
+print("Websocket: ws://localhost:%s" % (PORT))
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
