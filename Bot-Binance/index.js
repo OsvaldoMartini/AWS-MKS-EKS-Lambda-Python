216ea77057525b2f97b38f9aa3344d12024e3d15
Osvaldo Martini <martinio@dhl.com>
2023-12-13 19:51:40 +0100

    Fixin Margin for Future

--- Bot-Binance/index.js
+++ Bot-Binance/index.js
@@ -7,10 +7,10 @@ const tick = async (config, binanceClient) => {
   const { asset, base, spred, allocation } = config;
   const market = `${asset}/${base}`;
 
-  // const orders = await binanceClient.fetchOpenOrders(market);
-  // orders.forEach(async (order) => {
-  //   await binanceClient.cancelOrder(order.id);
-  // });
+  const orders = await binanceClient.fetchOpenOrders(market);
+  orders.forEach(async (order) => {
+    await binanceClient.cancelOrder(order.id);
+  });
 
   const result = await Promise.all([
     axios.get(
@@ -30,31 +30,33 @@ const tick = async (config, binanceClient) => {
   // Return all the Balance for all Cryptocurrencies
   const balances = await binanceClient.fetchBalance();
   // Extract the Bitcoin
-  const assetBalance = balances.free[asset]; // Initila balance of Bitcoin
+  const assetBalance = balances.free[asset]; // Initial balance of Bitcoin or USDT
   // Extract Tether
   const baseBalance = balances.free[base]; // Initial balance of Tether
   //Calculates the Sell Volume
-  const sellVolume = assetBalance * allocation;
+  const sellVolume =
+    assetBalance > 0 ? assetBalance * allocation : baseBalance * allocation;
+
   //Calculates the Buy Volume
   const buyVolume = (baseBalance * allocation) / marketPrice;
-
-  // Sending the Order to Binance
-  await binanceClient.createLimitSellOrder(market, sellVolume, sellPrice);
-  await binanceClient.createLimitBuyOrder(market, buyVolume, buyPrice);
 
   console.log(`
   	New tick for ${market}
   	Created limit sell order for ${sellVolume}@${sellPrice}
   	Created limit buy order for ${sellVolume}@${buyPrice}
   `);
+
+  // Sending the Order to Binance
+  await binanceClient.createLimitSellOrder(market, sellVolume, sellPrice);
+  await binanceClient.createLimitBuyOrder(market, buyVolume, buyPrice);
 };
 
 const run = () => {
   const config = {
     asset: "BTC",
     base: "USDT",
-    allocation: 0.1, //Percentage of our  portfolio to allocate for each trade
-    spred: 0.005, //Spread Percentage mid rate Buy or Sell limit order example:  10.000 our sale limit will be 12.000 and buy order will be 8.000
+    allocation: 0.02, //Percentage of our  portfolio to allocate for each trade
+    spred: 0.05, //Spread Percentage mid rate Buy or Sell limit order example:  10.000 our sale limit will be 12.000 and buy order will be 8.000
     tickInterval: 2000, // every 2 seconds evaluate  goig to sell or buy the limit order of the preious ticket  and create new one
   };
 
@@ -62,9 +64,12 @@ const run = () => {
     apiKey: process.env.API_KEY,
     secret: process.env.API_SECRET,
     verbose: true,
-    defaultType: "future",
     headers: {
       "X-MBX-APIKEY": process.env.API_KEY,
+    },
+    options: {
+      // defaultType: "spot",
+      defaultType: "future",
     },
   });
 
