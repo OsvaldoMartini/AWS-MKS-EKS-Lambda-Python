f6e9789edae3cd0bde2c97bd995190bef5632fb6
Osvaldo Martini <omartini@marginedge.com>
2024-04-15 02:04:06 +0200

    BUY IMBALANCE
    BUY IMBALANCE Control

---
+++ Binance-WebSoccket/rsibot/dynamic_last_5.py
@@ -0,0 +1,33 @@
+class LastFiveStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        if len(self.stack) >= 5:
+            self.pop_until(value)
+        else:
+            self.insert_sorted(value)
+
+    def insert_sorted(self, value):
+        index = 0
+        while index < len(self.stack) and self.stack[index] < value:
+            index += 1
+        self.stack.insert(index, value)
+
+    def pop_until(self, value):
+        while self.stack and self.stack[-1] < value and len(self.stack) >= 5:
+            self.stack.pop()
+        self.insert_sorted(value)
+
+    def get_values(self):
+        return self.stack
+
+# Example usage:
+last_five = LastFiveStack()
+values = [5, 2, 7, 3, 1, 4, 6]
+
+for value in values:
+    last_five.push(value)
+
+print("Last five values in ascending order:", last_five.get_values())
+print("Last :", last_five.get_values()[-2])
