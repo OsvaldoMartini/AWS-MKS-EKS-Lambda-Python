541a0e777e6cb56c052add69ee5423e58532c3fc
Osvaldo Martini <omartini@marginedge.com>
2024-05-01 20:39:50 +0200

    Update call_bots.bat

--- Binance-WebSoccket/rsibot/call_bots.bat
+++ Binance-WebSoccket/rsibot/call_bots.bat
@@ -2,8 +2,8 @@
 SETLOCAL
 
 cd D:\Projects DevOps\AWS MKS EKS Lambda Python\Binance-WebSoccket\rsibot
-start "BOT SPOT"  python bot-future-BTC-USDT.py
-start "BOT FUTURE" python bot-future-BTC-USDT-v1.2.py
+start "BOT FUTURE"  python bot-future-BTC-USDT.py
+start "BOT SPOT v1.2" python bot-future-BTC-USDT-v1.2.py
 
 
 ENDLOCAL
